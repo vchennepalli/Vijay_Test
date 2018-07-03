c9f09214432e426acf9e07c24b0c559a27e3cec6
vijaychennepalli <36756815+vijaychennepalli@users.noreply.github.com>
2018-07-03 18:18:29 +0530

    Create unit_test_script.py

---
+++ unit_test/unit_test_script.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
+from hello_world import sum
+
+print "############# Unit Test Script"
+
+print "############# Test case 1"
+total = sum(3, 4)
+print "Total:", total
+
+
+print "############# Test case 2"
+total = sum(10, 4)
+print "Total:", total
